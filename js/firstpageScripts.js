bf62c40c866227748411fc068ba9e9c21c6a07b4
ThomasAuriel <thomas.auriel.w@gmail.com>
2017-04-14 13:07:11 +0000

    Separate the title and author element from the body tag to place it in the handbook file. Add subsections.

--- js/firstpageScripts.js
+++ js/firstpageScripts.js
@@ -2,12 +2,12 @@ function formatFirstPage(firstPage){
 
 	var title = $('<h1>')
 	title.addClass('first-page-title')
-	title.append('Handbook <br>' + $('body')[0].getAttribute('title'))
+	title.append('Handbook <br>' + $('.handbook')[0].getAttribute('title'))
 	$(firstPage).append(title)
 
 	var author = $('<h2>')
 	author.addClass('first-page-author')
-	author.append($('body')[0].getAttribute('author'))
+	author.append($('.handbook')[0].getAttribute('author'))
 	$(firstPage).append(author)
 	
 }
